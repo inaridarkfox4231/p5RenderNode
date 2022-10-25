b3ee0bdabd56207152f4d279727eb264e1f9f3c4
inaridarkfox4231 <sea_gull6389-4m@ka.tnc.ne.jp>
2022-10-26 01:15:19 +0900

    予定変更で次はlilでライティングをいじる

--- src/test23.js
+++ src/test23.js
@@ -1,1 +1,31 @@
 // depthをテクスチャとして設定して通常の計算方法と比較
+// 何が入ってるのか見たい
+// 急遽予定変更でlil.guiに慣れたいです
+// 最終的にメソッド化して道具にする
+// てか両方でいいのでは？
+
+// 何かめんどくさくなってきたな...
+// どうもdepth textureめんどくさいようなので保留。
+
+// lil.guiに慣れよう！！というわけで。
+// ライティングにlilを組み合わせてトーラスで実験するか。よし。（結局、そうなる...）
+
+let ex = p5wgex;
+const _node;
+
+const vs = "";
+const fs = "";
+
+const gui = (function(){
+  const gui = new GUI();
+})();
+
+function setup(){
+  createCanvas(800, 640, WEBGL);
+  _node = new RenderNode(this._renderer.GL);
+
+}
+
+function draw(){
+
+}
